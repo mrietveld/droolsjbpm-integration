e181d66e81c2c7c869e71d4cdf5fd112554f1ce2
kiereleaseuser <kiereleaseuser@gmail.com>
2014-03-24 23:25:53 +0100

    Fixing compilation errors after cherry-picks.

--- kie-remote/kie-services-remote/src/test/java/org/kie/services/remote/jms/JmsTaskResourceAndDeploymentIdTest.java
+++ kie-remote/kie-services-remote/src/test/java/org/kie/services/remote/jms/JmsTaskResourceAndDeploymentIdTest.java
@@ -18,6 +18,10 @@ import org.kie.services.client.serialization.jaxb.rest.JaxbExceptionResponse;
 import org.kie.services.remote.TaskDeploymentIdTest;
 import org.kie.services.remote.cdi.DeploymentInfoBean;
 import org.kie.services.remote.cdi.ProcessRequestBean;
+import org.jbpm.process.audit.command.FindProcessInstancesCommand;
+import org.kie.services.client.serialization.jaxb.impl.JaxbCommandResponse;
+import org.jbpm.process.audit.command.ClearHistoryLogsCommand;
+
 
 @SuppressWarnings("unchecked")
 public class JmsTaskResourceAndDeploymentIdTest extends RequestMessageBean implements TaskDeploymentIdTest {
@@ -79,34 +83,6 @@ public class JmsTaskResourceAndDeploymentIdTest extends RequestMessageBean imple
         verify(injectedTaskService, times(1)).getTaskById(eq(TASK_ID));
         // complete operation should be done by runtime task service
         verify(runtimeTaskService, times(1)).execute(any(TaskCommand.class));
-    }
-
-    @Test
-    public void testJmsAuditCommandWithoutDeploymentId() {
-        setupTaskMocks(this, FOR_PROCESS_TASKS);
-
-        // run cmd (no deploymentId set on JaxbConmandsRequest object
-        JaxbCommandsRequest 
-        cmdsRequest = new JaxbCommandsRequest(new FindProcessInstancesCommand());
-        JaxbCommandsResponse 
-        response = this.processJaxbCommandsRequest(cmdsRequest);
-       
-        // check result
-        assertEquals( "Number of response objects", 1, response.getResponses().size() );
-        JaxbCommandResponse<?> 
-        responseObj = response.getResponses().get(0);
-        assertFalse( "Command did not complete successfully", responseObj instanceof JaxbExceptionResponse );
-        
-        // run cmd (no deploymentId set on JaxbConmandsRequest object
-        cmdsRequest = new JaxbCommandsRequest(new ClearHistoryLogsCommand());
-        response = this.processJaxbCommandsRequest(cmdsRequest);
-        
-        // check result
-        assertEquals( "Number of response objects", 0, response.getResponses().size() );
-        
-        // verify
-        verify(auditLogService, times(1)).findProcessInstances();
-        verify(auditLogService, times(1)).clear();
     }
 
 }
